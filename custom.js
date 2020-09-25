d86d0633cbade7e60f3b5908cb1cebf66a094c8a
Natemafe <natemafe@gmail.com>
2020-09-25 10:01:49 +0200

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,7 @@
+let mainNav = document.getElementById ('mainNav');
+let navBarToggle = document.getElementById('navToggle');
+
+navBarToggle.addEventListener('click', function () {
+	mainNav.classList.toggle('active');
+});
+
